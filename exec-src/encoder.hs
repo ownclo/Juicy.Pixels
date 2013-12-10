7d832507900a5cf9b7fa11b684f08614e9b617d9
Ownclo - Arcis <mrownclo@gmail.com>
2013-12-10 18:19:50 +0400

    Provided simple encoder (BMP -> JPEG)

---
+++ exec-src/encoder.hs
@@ -0,0 +1,17 @@
+import Codec.Picture.Saving
+import Codec.Picture.Jpg( encodeJpeg )
+import Codec.Picture.Bitmap( decodeBitmap )
+
+import System.Environment( getArgs )
+import Control.Applicative( (<$>) )
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+    [inBmp, outJPG] <- getArgs
+    bmpData <- B.readFile inBmp
+
+    let (Right jpgData) = imageToJpg 50 <$> decodeBitmap bmpData
+    L.writeFile outJPG jpgData
